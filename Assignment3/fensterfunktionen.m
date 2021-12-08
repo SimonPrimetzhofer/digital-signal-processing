fd10700c293d3c377c0b4899858f36c1521c5413
Simon Primetzhofer <simon.primetzhofer@live.at>
2021-12-08 19:34:28 +0100

    assignment3 ex4

--- Assignment3/fensterfunktionen.m
+++ Assignment3/fensterfunktionen.m
@@ -56,6 +56,30 @@ fftBartlettWeighted = 20*log10(abs(fftshift(fft(xSin_bartlettWindow, N))));
 fftHanningWeighted = 20*log10(abs(fftshift(fft(xSin_hanningWindow, N))));
 fftBlackmanWeighted = 20*log10(abs(fftshift(fft(xSin_blackmanWindow, N))));
 
+% more sinus signals
+x1Sin = sin(2 * pi * 48 * fvec) + sin(2 * pi * 52 * fvec);
+x2Sin = sin(2 * pi * 50 * fvec) + 0.1*sin(2 * pi * 60 * fvec); 
+
+x1Sin_rectWindow = x1Sin .* sinRect';
+x1Sin_bartlettWindow = x1Sin .* sinBartlett';
+x1Sin_hanningWindow = x1Sin .* sinHanning';
+x1Sin_blackmanWindow = x1Sin .* sinBlackman';
+
+x2Sin_rectWindow = x2Sin .* sinRect';
+x2Sin_bartlettWindow = x2Sin .* sinBartlett';
+x2Sin_hanningWindow = x2Sin .* sinHanning';
+x2Sin_blackmanWindow = x2Sin .* sinBlackman';
+
+fftRectWeighted2 = 20*log10(abs(fftshift(fft(x1Sin_rectWindow, N))));
+fftBartlettWeighted2 = 20*log10(abs(fftshift(fft(x1Sin_bartlettWindow, N))));
+fftHanningWeighted2 = 20*log10(abs(fftshift(fft(x1Sin_hanningWindow, N))));
+fftBlackmanWeighted2 = 20*log10(abs(fftshift(fft(x1Sin_blackmanWindow, N))));
+
+fftRectWeighted3 = 20*log10(abs(fftshift(fft(x2Sin_rectWindow, N))));
+fftBartlettWeighted3 = 20*log10(abs(fftshift(fft(x2Sin_bartlettWindow, N))));
+fftHanningWeighted3 = 20*log10(abs(fftshift(fft(x2Sin_hanningWindow, N))));
+fftBlackmanWeighted3 = 20*log10(abs(fftshift(fft(x2Sin_blackmanWindow, N))));
+
 % Visualization
 % a)
 figure('name', 'Assignment 3 a) b)');
@@ -109,9 +133,9 @@ title('Blackman');
 xlabel('\Omega');
 ylabel('Magnitude in dB');
 
-% d)
+% d) and e)
 % weighted signals
-figure('name', 'Assignment 3 d)');
+figure('name', 'Assignment 3 d) and e)');
 subplot(311); hold on; grid on;
 plot(fvec, xSin);
 xlim([0,0.5]);
@@ -146,6 +170,74 @@ plot(tt,fftHanningWeighted, 'color', 'g');
 
 subplot(313); hold on; grid on;
 plot(tt,fftBlackmanWeighted, 'color', 'b');
+legend('Weighted Rectangular', 'Weighted Bartlett', 'Weighted Hanning', 'Weighted Blackman');
+xlabel('\Omega');
+ylabel('Magnitude in dB');
+title('Value of weighted windows');
+
+% f)
+% weighted signals
+figure('name', 'Assignment 3 f) i)');
+subplot(211); hold on; grid on;
+plot(x1Sin_bartlettWindow, 'color', 'k');
+
+subplot(211); hold on; grid on;
+plot(x1Sin_hanningWindow, 'color', 'r');
+
+subplot(211); hold on; grid on;
+plot(x1Sin_blackmanWindow, 'color', 'g');
+
+subplot(211); hold on; grid on;
+plot(x1Sin_blackmanWindow, 'color', 'b');
+legend('Weighted Rectangular', 'Weighted Bartlett', 'Weighted Hanning', 'Weighted Blackman');
+xlabel('Samples');
+ylabel('Amplitude');
+title('Weighted windows with sinus');
+
+subplot(212); hold on; grid on;
+plot(tt,fftRectWeighted2, 'color', 'k');
+
+subplot(212); hold on; grid on;
+plot(tt,fftBartlettWeighted2, 'color', 'r');
+
+subplot(212); hold on; grid on;
+plot(tt,fftHanningWeighted2, 'color', 'g');
+
+subplot(212); hold on; grid on;
+plot(tt,fftBlackmanWeighted2, 'color', 'b');
+legend('Weighted Rectangular', 'Weighted Bartlett', 'Weighted Hanning', 'Weighted Blackman');
+xlabel('\Omega');
+ylabel('Magnitude in dB');
+title('Value of weighted windows');
+
+figure('name', 'Assignment 3 f) ii)');
+subplot(211); hold on; grid on;
+plot(x2Sin_bartlettWindow, 'color', 'k');
+
+subplot(211); hold on; grid on;
+plot(x2Sin_hanningWindow, 'color', 'r');
+
+subplot(211); hold on; grid on;
+plot(x2Sin_blackmanWindow, 'color', 'g');
+
+subplot(211); hold on; grid on;
+plot(x2Sin_blackmanWindow, 'color', 'b');
+legend('Weighted Rectangular', 'Weighted Bartlett', 'Weighted Hanning', 'Weighted Blackman');
+xlabel('Samples');
+ylabel('Amplitude');
+title('Weighted windows with sinus');
+
+subplot(212); hold on; grid on;
+plot(tt,fftRectWeighted3, 'color', 'k');
+
+subplot(212); hold on; grid on;
+plot(tt,fftBartlettWeighted3, 'color', 'r');
+
+subplot(212); hold on; grid on;
+plot(tt,fftHanningWeighted3, 'color', 'g');
+
+subplot(212); hold on; grid on;
+plot(tt,fftBlackmanWeighted3, 'color', 'b');
 legend('Weighted Rectangular', 'Weighted Bartlett', 'Weighted Hanning', 'Weighted Blackman');
 xlabel('\Omega');
 ylabel('Magnitude in dB');
