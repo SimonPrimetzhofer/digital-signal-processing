1980af274cd19b826d9be65ccc530118e9e05544
KaneAbi <kaan.baylan28@gmail.com>
2022-01-07 15:13:21 +0100

    Create ex2.m
    As4: ex2

---
+++ Assignment4/ex2.m
@@ -0,0 +1,59 @@
+clear
+close all
+clc 
+
+x = chirp((0:511),0,511,0.02);
+h = hanning(128)';
+h = h/sum(h);
+L = 256;
+
+lh = length(h);
+lx = length(x);
+
+r = rem(lx,L);
+
+% b) FFT Zero Padding von h
+hb = [h zeros(1, L-1)];
+
+H = fft(hb, L);
+
+% c) FFT Zero Padding von x
+xb = [x zeros(1, L-r)];
+
+X = fft(xb, L);
+
+% d) Faltung und die Ausgangssignale berechnen
+nr = length(xb)/L;
+
+for k = 1:nr
+   M1(k,:) = xb(((k-1) * L+1):k * L);
+   M2(k,:) = [M1(k,:) zeros(1, lh-1)];
+   M3(k,:) = ifft(fft(M2(k,:)) .* fft(hb));
+   M4(k,:) = [zeros(1, (k-1)*L) M3(k,:) zeros(1, (nr-k) * L)];
+end
+
+z = sum(M4);
+
+% f)
+con = conv(x,h);
+
+
+subplot(4,1,1);
+stem(X);
+title("Eingangssignal");
+
+subplot(4,1,2);
+stem(H);
+title("Impulsantwort");
+
+subplot(4,1,3);
+stem(z);
+title("Ausgangssignal");
+xlim([0 1000]);
+
+subplot(4,1,4);
+stem(con);
+title("conv");
+xlim([0 1000]);
+
+
